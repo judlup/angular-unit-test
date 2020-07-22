eff6fdcba7d0c43fabd329a2357d68573c358bb7
Judlup Luns <judlup@gmail.com>
2020-07-22 00:43:26 -0500

    22072020

--- src/app/formulario/formulario.component.spec.ts
+++ src/app/formulario/formulario.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormularioComponent } from './formulario.component';
+import { CommonModule } from '@angular/common';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
@@ -18,18 +19,34 @@ describe('FormularioComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormularioComponent);
     component = fixture.componentInstance;
+    component.ngOnInit(); 
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 
-  it('should render select values', () => {
-    const fixture = TestBed.createComponent(FormularioComponent);
+  // it('should render select values', () => {
+  //   const fixture = TestBed.createComponent(FormularioComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('select').textContent).toContain('Value 2');
+  // });
+
+  it("Checkbox should not selected", () => {
+    expect(component.frmData.controls['chkb'].value).toBeFalsy();
+  })
+
+  it("Checkbox should not selected", () => {
+    let checkbox = component.frmData.controls['chkb'].setValue(true);
+    
+    // component.chkbstatus = component.frmData.controls['chkb'].value        
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('select').textContent).toContain('Value 2');
-  });
+    // expect(component.chkbstatus).toBeTruthy();    
+    // expect(component.frmData.controls['chkb'].value).toBeTruthy();    
+  })
+
+
 
 });
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -8,20 +8,45 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class FormularioComponent implements OnInit {
 
+  public frmData:FormGroup;
+  public d1:any;
+  public d2:any;
+  public d2dis:any = true;
+  public chkbstatus:boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
-  ) { }
-
-  public frmData:FormGroup;
+  ) { }  
 
   ngOnInit() {
     this.frmData = this.formBuilder.group({
-      nombre: ['', Validators.required],
+      // nombre: ['', Validators.required],
+      chkb: ['', Validators.required],
+      d1: ['', Validators.required],
+      d2: ['', Validators.required],
     });
   }
 
   sbm(){
-    console.log("Aqui");
+    let cb = this.frmData.value.chkb;
+    let d1 = this.frmData.value.d1;
+    let d2 = this.frmData.value.d2;      
+    if(this.frmData.valid){
+      console.log("ok");
+      return true;
+    }else{
+      console.log("fail");
+      return false;
+    }
+  }
+
+  DateSel(event){        
+    this.d1 = event.target.value;
+    this.d2dis = false;
+  }
+
+  chkbCh(event){
+    this.chkbstatus = event.target.checked;
   }
 
 }
